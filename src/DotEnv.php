5b06781a17cf9fe52f02670dd8d8eba21bf91373
Maxim Ageev <maxim.ageev@paxful.com>
2023-02-10 18:49:52 +0300

    Missing keys

--- src/DotEnv.php
+++ src/DotEnv.php
@@ -17,7 +17,7 @@ class DotEnv
     {
         $this->lines = explode(PHP_EOL, $content);
         foreach ($this->lines as $lineIndex => $line) {
-            if (str_starts_with( $line, '#') || empty($line)) {
+            if (empty($line) || str_starts_with( $line, '#')) {
                 continue;
             }
             [$key, $value] = explode('=', $line);
@@ -35,8 +35,19 @@ class DotEnv
     {
         $lines = $base->lines;
         foreach ($base->mapKeyLineNumber as $key => $lineNumber) {
+            if (!array_key_exists($key, $this->mapValueByKey)){
+                continue;
+            }
             $value = $this->mapValueByKey[$key];
             $lines[$base->mapKeyLineNumber[$key]] = sprintf('%s=%s', $key, $value);
+        }
+        $diffKeys = array_diff_key($this->mapValueByKey, $base->mapValueByKey);
+
+        if (!empty($diffKeys)) {
+            $lines[] = '# Missing keys in base';
+            foreach ($diffKeys as $key => $value) {
+                $lines[] = sprintf('%s=%s', $key, $value);
+            }
         }
 
         return new DotEnv(implode(PHP_EOL, $lines));
